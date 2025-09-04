d439fadc8858037835a10b6b3c62543aefa925d6
agb00033 <147870922+agb00033@users.noreply.github.com>
2025-09-04 09:31:21 -0400

    Fixing pdfjs for vercel

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -3,11 +3,14 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-// Setup for the PDF.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+// // Setup for the PDF.js worker
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   'pdfjs-dist/build/pdf.worker.min.mjs',
+//   import.meta.url,
+// ).toString();
 
 function Experience() {
   const [numPages, setNumPages] = useState(null);
