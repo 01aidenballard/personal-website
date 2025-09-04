85a731076dade2fb6f460035369011795650514c
agb00033 <147870922+agb00033@users.noreply.github.com>
2025-09-04 09:58:03 -0400

    I'm tired of this

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
-import pdfWorker from "pdfjs-dist/build/pdf.worker.min.mjs?url";
+import pdfWorker from "pdfjs-dist/build/pdf.worker.min.mjs?worker";
 
 pdfjs.GlobalWorkerOptions.workerSrc = pdfWorker;
 
